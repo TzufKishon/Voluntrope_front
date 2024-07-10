d60f2883ee0f7864b86a1c9b76d912ceeb316667
Tzuf Kishon <108464777+TzufKishon@users.noreply.github.com>
2024-07-10 11:45:43 +0300

    CartContext.js

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,37 +1,49 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({});
 
-export function CartContextProvider({children}) {
-  const ls = typeof window !== "undefined" ? window.localStorage : null;
-  const [cartProducts,setCartProducts] = useState([]);
+export function CartContextProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState([]);
+
+  // Use useEffect to set the state from localStorage after the component mounts
   useEffect(() => {
-    if (cartProducts?.length > 0) {
-      ls?.setItem('cart', JSON.stringify(cartProducts));
-    }
-  }, [cartProducts]);
-  useEffect(() => {
+    const ls = typeof window !== "undefined" ? window.localStorage : null;
     if (ls && ls.getItem('cart')) {
       setCartProducts(JSON.parse(ls.getItem('cart')));
     }
   }, []);
+
+  useEffect(() => {
+    const ls = typeof window !== "undefined" ? window.localStorage : null;
+    if (ls) {
+      if (cartProducts.length === 0) {
+        ls.removeItem('cart');
+      } else {
+        ls.setItem('cart', JSON.stringify(cartProducts));
+      }
+    }
+  }, [cartProducts]);
+
   function addProduct(productId) {
-    setCartProducts(prev => [...prev,productId]);
+    setCartProducts(prev => [...prev, productId]);
   }
+
   function removeProduct(productId) {
     setCartProducts(prev => {
       const pos = prev.indexOf(productId);
       if (pos !== -1) {
-        return prev.filter((value,index) => index !== pos);
+        return prev.filter((value, index) => index !== pos);
       }
       return prev;
     });
   }
+
   function clearCart() {
     setCartProducts([]);
   }
+
   return (
-    <CartContext.Provider value={{cartProducts,setCartProducts,addProduct,removeProduct,clearCart}}>
+    <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct, removeProduct, clearCart }}>
       {children}
     </CartContext.Provider>
   );
